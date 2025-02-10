60e73b0bd2fd11b459a08a1d72992274373eb712
Belouahed <100034909+Belouahed@users.noreply.github.com>
2025-02-10 23:12:36 +0100

    Upload backend to GitHub

---
+++ server.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const User = require("./models/User");
+
+app.post("/users", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const newUser = new User({ name, email });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: "Error adding user" });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching users" });
+  }
+});
+
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ Connected to MongoDB"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+  res.send("🚀 API is running...");
+});
+
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
