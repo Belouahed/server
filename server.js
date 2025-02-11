d13ece1a3197b41fe23fcf7baea942be0112e0bd
Belouahed <100034909+Belouahed@users.noreply.github.com>
2025-02-12 00:11:37 +0100

    update cors

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.use(cors({
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
 }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const User = require("./models/User");
 
 app.post("/users", async (req, res) => {
